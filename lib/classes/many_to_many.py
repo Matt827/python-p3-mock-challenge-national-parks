6e9a8ca26fc48886384ac070bcc8b9daceb62a87
Matthew <matthew.stewart827@gmail.com>
2023-09-20 14:57:40 -0500

    added all properties to classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,6 +3,15 @@ class NationalPark:
     def __init__(self, name):
         self.name = name
         
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 3 and not hasattr(self, "name"):
+            self._name = name
+            
     def trips(self):
         pass
     
@@ -16,6 +25,7 @@ class NationalPark:
         pass
 
 
+
 class Trip:
     
     def __init__(self, visitor, national_park, start_date, end_date):
@@ -23,12 +33,57 @@ class Trip:
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
-
+        
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
+        
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, end_date):
+        if isinstance(end_date, str) and len(end_date) >= 7:
+            self._end_date = end_date
+            
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        if isinstance(visitor, Visitor):
+            self._visitor = visitor
+            
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, national_park):
+        if isinstance(national_park, NationalPark):
+            self._national_park = national_park
+    
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
         
     def trips(self):
         pass
--- lib/debug.py
+++ lib/debug.py
@@ -7,5 +7,11 @@ from classes.many_to_many import Trip
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
+    
+    p_1 = NationalPark("Yosemite")
+    vis_1 = Visitor("Sheryl")
+    trip_1 = Trip(vis_1, p_1, "May 5th", "May 9th")
+    Trip(vis_1, p_1, "June 20th", "July 4th")
+    Trip(vis_1, p_1, "January 5th", "January 20th")
 
     ipdb.set_trace()
